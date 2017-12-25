fde1bf0721cfda893003f45023941b6c4ae0006f
zhou yihang <zhouyihangok@163.com>
2017-12-25 14:17:55 +0800

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import FlaskApp.views
 from WeiXinCore.WeiXin import echo
 try:
-    import sae
-    import FlaskApp.sae_py
+    import os
+    import FlaskApp.os_py
 except:
-    print("no in sae.")
+    print("no in os.")
