7d2091f43b055887addd2150229e09f9443f48db
zhou yihang <zhouyihangok@163.com>
2017-12-26 11:58:56 +0800

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import FlaskApp.views
 from WeiXinCore.WeiXin import echo
 #try:
-    import os
-    import FlaskApp.os_py
+#import os
+import FlaskApp.os_py
 #except:
 #    print("no in os.")
