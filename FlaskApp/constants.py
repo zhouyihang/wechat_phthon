3dac56aaf8075e6fe55e18ba4364a8cf865d0ffe
zhou yihang <zhouyihangok@163.com>
2017-12-27 18:43:30 +0800

    Update constants.py

--- FlaskApp/constants.py
+++ FlaskApp/constants.py
@@ -1,4 +1,5 @@
 import os
 #MYSQL_URI = 'mysql://'+os.environ['OPENSHIFT_MYSQL_DB_USERNAME']+':'+os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']+"@"+os.environ['OPENSHIFT_MYSQL_DB_HOST']+':'+os.environ['OPENSHIFT_MYSQL_DB_PORT']
-MYSQL_URI = os.environ['OPENSHIFT_MYSQL_DB_URL']+os.environ['OPENSHIFT_APP_NAME']
+#MYSQL_URI = os.environ['OPENSHIFT_MYSQL_DB_URL']+os.environ['OPENSHIFT_APP_NAME']
+MYSQL_URI = 'mysql://172.30.244.114:3306'
 #MYSQL_URI = 'mysql://root:@localhost/scheduler'
