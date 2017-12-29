bffb994ca86f208ae47cf71d6518419d86030d34
zhou yihang <zhouyihangok@163.com>
2017-12-29 18:15:40 +0800

    Update views.py

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -3,9 +3,9 @@ from FlaskApp import app
 
 
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+#@app.route('/')
+#def index():
+#    return "Hello, World!"
 
 # @app.route('/wx', methods = ['GET', 'POST'] )
 # def wx():
@@ -14,3 +14,25 @@ def index():
 #     if not check_signature(request.args) and not app.debug:
 #         return ""
 #     return echo
+
+@app.route('/',methods=['GET','POST'])
+def wechat():
+if request.method=='GET':
+token='weixin'
+data=request.args
+signature=data.get('signature','')
+timestamp=data.get('timestamp','')
+nonce =data.get('nonce','')
+echostr=data.get('echostr','')
+s=[timestamp,nonce,token]
+s.sort()
+s=''.join(s)
+if(hashlib.sha1(s).hexdigest()==signature):
+return make_response(echostr)
+else:
+rec=request.stream.read()
+xml_rec=ET.fromstring(rec)
+tou = xml_rec.find('ToUserName').text
+fromu = xml_rec.find('FromUserName').text
+content = xml_rec.find('Content').text
+xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
