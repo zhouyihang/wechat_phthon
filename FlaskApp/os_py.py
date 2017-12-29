63746f3b1f0d5adaeca4fa866c8db8582081ebb8
zhou yihang <zhouyihangok@163.com>
2017-12-29 11:50:53 +0800

    Update os_py.py

--- FlaskApp/os_py.py
+++ FlaskApp/os_py.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, g, request
 from FlaskApp import  app
-app = Flask(__name__)
+#app = Flask(__name__)
 app.debug = True
 
 @app.route('/douban', methods=['GET', 'POST'])
