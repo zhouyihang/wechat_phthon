30f25f156af5ca78bf898e7b9dd989ad0cd241d8
zhou yihang <zhouyihangok@163.com>
2017-12-29 14:27:27 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
+#
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
+#    
+#from WeiXinCore.WeiXin import echo
+#
+#from flask import Flask, g, request
+#
+#@app.route('/douban', methods=['GET', 'POST'])
+#def douban():
+#    return str( dict(request.args))
+#  
+#@app.route('/fankui', methods=['GET', 'POST'])
+#def greeting():
+#    html = ''
+#
+#    return html
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-    
-from WeiXinCore.WeiXin import echo
-
-from flask import Flask, g, request
-
-@app.route('/douban', methods=['GET', 'POST'])
-def douban():
-    return str( dict(request.args))
-  
-@app.route('/fankui', methods=['GET', 'POST'])
-def greeting():
-    html = ''
-
-    return html
+from FlaskApp import app, os_py
 
 if __name__ == "__main__":
     application.run()
