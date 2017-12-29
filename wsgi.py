45251fcc55979f9f72e1b66b7145a58efc09099c
zhou yihang <zhouyihangok@163.com>
2017-12-29 11:06:32 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,11 @@
-from flaskapp import app as application
+import os
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def root():
+    return("Hello there")
+
+if __name__ == "__main__":
+    application.run()
