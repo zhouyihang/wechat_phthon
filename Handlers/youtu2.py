b2d2af440bb904334c94be1c9bd0fde37bbd805b
zhou yihang <zhouyihangok@163.com>
2018-01-17 15:08:04 +0800

    Create youtu2.py

---
+++ Handlers/youtu2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# http://open.youtu.qq.com/#/develop/api-ocr-general
+# github.com/se4
+
+import os
+import time
+import random
+import hmac
+import hashlib
+import binascii
+import base64
+import requests
+import json
+
+
+def cal_sig():
+    timestamp = int(time.time())
+    expired = str(timestamp + 2592000)
+    rdm = str(random.randint(0, 999999999))
+    plain_text = 'a={appid}&k={secret_id}&e={expired}&t={timestamp}&r={rdm}&u={qq}&f='
+    plain_text = plain_text.format(appid=youtu_app_id,
+                                   secret_id=youtu_secret_id,
+                                   timestamp=timestamp,
+                                   rdm=rdm, qq=youtu_qq,
+                                   expired=expired)
+    bin = hmac.new(youtu_secret_key.encode(), plain_text.encode(), hashlib.sha1).hexdigest()
+    s = binascii.unhexlify(bin)
+    s = s + plain_text.encode('ascii')
+    signature = base64.b64encode(s).rstrip().decode()
+    return signature
+
+def get_text(image_path):
+    signature = cal_sig()
+    headers = {'Host': 'api.youtu.qq.com', 'Content-Type': 'text/json', 'Authorization': signature}
+    filepath = os.path.abspath(image_path)
+    data = {'app_id': youtu_app_id, 'image': ''}
+    data['image'] = base64.b64encode(open(filepath, 'rb').read()).rstrip().decode('utf-8')
+    resp = requests.post('https://api.youtu.qq.com/youtu/ocrapi/generalocr',
+                         data=json.dumps(data),
+                         headers=headers)
+    if 'items' in resp.text:
+        return resp.content.decode('utf-8')
+    else:
+        return '0'
