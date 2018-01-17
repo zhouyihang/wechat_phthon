aacadb1d3d8461f8bf3172c6dd865363a0069139
zhou yihang <zhouyihangok@163.com>
2018-01-17 11:50:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+youto_app_id = os.environ['OPENSHIFT_ENV_YOUTO_APP_ID']
+youto_secret_id = os.environ['OPENSHIFT_ENV_YOUTO_SECRET_ID']
+youto_secret_key = os.environ['OPENSHIFT_ENV_YOUTO_SECRET_KEY']
+youto_qq = os.environ['OPENSHIFT_ENV_QQ']
